573874edd753067da62a3acb60498d2df2c67bcb
kszewc <kszewc@fizyka.umk.pl>
2025-09-12 07:24:23 +0200

    Temporarly added run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+
+python multiSMD.py TEST/example.pdb TEST/example.psf TEST/example.vel TEST/example.coor TEST/example.xsc TEST/toppar.zip TEST/template.inp TEST/template.run 'name CA and protein and chainID S' 'name CA and chainID N' 3
